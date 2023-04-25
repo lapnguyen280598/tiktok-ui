7d74e9313501e5c519d74149cc7ebd0983b6a1b2
Lap Nguyen <lapnguyen.280598@gmail.com>
2023-04-25 08:53:06 +0700

    header-1

---
+++ src/accest/image/index.js
@@ -0,0 +1,5 @@
+const image = {
+    logo: require("~/accest/image/logo.svg").default
+}
+
+export default image
--- src/components/Layout/component/Header/index.js
+++ src/components/Layout/component/Header/index.js
@@ -1,12 +1,35 @@
 import className from "classnames/bind"
 import styles from "./Header.module.scss"
+import image from "~/accest/image"
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faCircleXmark, } from "@fortawesome/free-solid-svg-icons"
+import { faMagnifyingGlass, faSpinner } from "@fortawesome/free-solid-svg-icons"
+
 
 const cx = className.bind(styles)
+
+console.log(image.logo)
 
 function Header() {
     return <header className={cx("wrapper")}>
         <div className={cx("inner")}>
-
+            <div className={cx("logo")}>
+                <img src={image.logo} alt="logo" />
+            </div>
+            <div className={cx("mid")}>
+                <input placeholder="Tìm kiếm" />
+                <button className={cx("close")}>
+                    <FontAwesomeIcon icon={faCircleXmark} />
+                </button>
+                <FontAwesomeIcon className={cx("loading")} icon={faSpinner} />
+                <button className={cx("search")}>
+                    <FontAwesomeIcon icon={faMagnifyingGlass} />
+                </button>
+            </div>
+            <div className={cx("actions")}>
+                a
+            </div>
         </div>
     </header>
 
